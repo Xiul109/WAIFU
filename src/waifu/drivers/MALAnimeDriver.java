8d93dc96205c1b87bc0684843c2e3cc7b83eb50d
Iván García <ciberpatro@gmail.com>
2016-11-06 02:23:18 +0100

    Ahora mira todas las páginas y la duración

--- src/waifu/drivers/MALAnimeDriver.java
+++ src/waifu/drivers/MALAnimeDriver.java
@@ -12,42 +12,111 @@ import com.jaunt.UserAgent;
 
 public class MALAnimeDriver implements AnimeDriver{
 	private List<Element> animes;
+	private Element pageAc;
+	private boolean hasNextPageOptions;
+	private UserAgent ua= new UserAgent();
+	
 	public MALAnimeDriver(String tag, String url) throws ResponseException, NotFound{
-		UserAgent ua= new UserAgent();
 		ua.visit(url);
 		List<Element> tagGroup = ua.doc.findFirst("<div class=\"genre-link\">").getChildElements();
 		for (Element i:tagGroup)
 			for(Element j:i.getChildElements())
-				if (j.getChildElements().get(0).getText().contains(tag))
-					url=j.getChildElements().get(0).getAt("href");
+				if (j.getElement(0).getText().contains(tag))
+					url=j.getElement(0).getAt("href");
 		ua.visit(url);
 		animes=ua.doc.findFirst("<div class=\"seasonal-anime-list clearfix\">").getChildElements();
+		hasNextPageOptions=true;
+		try{
+			pageAc=ua.doc.findFirst("<div class=\"pagination ac\">").getFirst("<a class=\"link current\"");
+		}catch (NotFound e){
+			hasNextPageOptions=false;
+		}
 	}
 	
-	
-	public Anime next(){
-		
+	private Anime getAnime()throws NotFound, ResponseException{
 		String name="unknown";
 		String synopsis="unknown";
 		List<String> tags=new LinkedList<String>();
 		int nChapters=-1;
 		int duration=-1;
 		float score=-1.0f;
-		Anime ani=null;
+		String animeUrl="";
+		UserAgent uaInfo=new UserAgent();
+		String score_aux="";
+		List<Element> info;
+		String nChapters_aux="";
+		
+		Element anime=animes.remove(0);
+		if (animes.size()==0&&hasNextPage()&&hasNextPageOptions){
+			String url=pageAc.nextSiblingElement().getAt("href");
+			ua.visit(url);
+			pageAc=ua.doc.findFirst("<div class=\"pagination ac\">").getFirst("<a class=\"link current\"");
+			animes=ua.doc.findFirst("<div class=\"seasonal-anime-list clearfix\">").getChildElements();
+		} 
+
+		name=anime.findFirst("<p class=\"title-text\">").getElement(0).getText();
+		
+		synopsis=anime.findFirst("<div class=\"synopsis js-synopsis\">").getElement(0).getText();
+		synopsis=synopsis.replaceAll("&#039;","\'").replaceAll("&quot;","\"");
+		
+		for (Element each_tag:anime.findEach("<span class=\"genre\">"))
+			tags.add(each_tag.getElement(0).getText());
+		
+		score_aux=anime.findFirst("<span class=\"score\">").getText().replaceAll("\\s+","");
+		score = score_aux.equals("N/A") ? -1.0f:Float.parseFloat(score_aux.replaceAll("[^0-9.]",""));
+		
+		animeUrl=anime.findFirst("<p class=\"title-text\">").getElement(0).getAt("href");
+		uaInfo.visit(animeUrl);
+		info=uaInfo.doc.findFirst("<div class=\"js-scrollfix-bottom\"").getChildElements();
+		
+		nChapters_aux=anime.findFirst("<div class=\"eps\">").getElement(0).getElement(0).getText();
+		nChapters_aux=nChapters_aux.replaceAll("\\s+","");
+		nChapters=nChapters_aux.contains("?") ?-1:cleanNumbers(nChapters_aux);
+		
+		for (Element labels:info){
+			for(Element label: labels.getChildElements()){
+				if (label.getText().equals("Duration:"))
+					duration=animeDuration(label.getParent().getText());
+			}
+		}
+		
+		return new Anime(name,synopsis,tags,nChapters,duration,score);
+	}
+	
+	private boolean hasNextPage(){
+		return (hasNextPageOptions&&pageAc.getSiblingIndex()!=pageAc.getParent().getChildElements().size()-1);
+	}
+	
+	private int animeDuration(String text){
+		int hr=0;
+		int min=0;
+		int total=0;
+		if (text.contains("hr.")){
+			hr=cleanNumbers(text.split(".hr")[0]);
+			min=cleanNumbers(text.split(".hr")[1]);
+		}else if (!text.contains("hr.")){
+			min=cleanNumbers(text.split("min.")[0]);
+		}
+		total=(hr*60)+min;
+		if (total==0) total=-1;
+		return total;
+	}
+	
+	private int cleanNumbers(String numbers){
+		int number=-1;
+		numbers=numbers.replaceAll("\\s+","");
+		if (!numbers.equals("Unknown")&&!numbers.equals("None"))
+			number=Integer.parseInt(numbers.replaceAll("[^0-9]",""));
+		return number;
+	}
+	
+	public Anime next(){
 		try {
 			if (animes.size()>0){
-				Element anime=animes.remove(0);
-				name=anime.findFirst("<p class=\"title-text\">").getChildElements().get(0).getText();
-				nChapters=Integer.parseInt(anime.findFirst("<div class=\"eps\">").getChildElements().get(0).getChildElements().get(0).getText().split(" ")[0]);
-				synopsis=anime.findFirst("<div class=\"synopsis js-synopsis\">").getChildElements().get(0).getText();
-				score=Float.parseFloat(anime.findFirst("<span class=\"score\">").getText().replaceAll("[^0-9.]",""));
-				for (Element each_tag:anime.findEach("<span class=\"genre\">"))
-					tags.add(each_tag.getChildElements().get(0).getText());
-			
-				ani=new Anime(name,synopsis,tags,nChapters,-1,score);
-			}
-		} catch (NotFound e) {}
-		return ani;
+				return getAnime();
+			} else return null;
+		} catch (NotFound | ResponseException e) {}
+		return null;
 	}
 	
 	public boolean hasNext(){
