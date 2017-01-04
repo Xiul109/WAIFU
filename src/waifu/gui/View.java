6d8309361096fef13b44b856bd2681be01912f79
xiul <xiul10901@gmail.com>
2017-01-04 13:11:07 +0100

    Añadidos métodos a la interfaz View

---
+++ src/waifu/gui/View.java
@@ -0,0 +1,9 @@
+package waifu.gui;
+
+import java.util.List;
+import waifu.Anime;
+
+public interface View{
+	public void giveTags(List<String> tags);
+	public void giveAnimes(List<Anime> animes);
+}
