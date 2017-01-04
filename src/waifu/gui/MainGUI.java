f808cedeb50e2bc83a1491cf458ab9d18c00e88b
xiul <xiul10901@gmail.com>
2017-01-04 13:11:46 +0100

    Creada la clase MainGUI con métodos básicos

---
+++ src/waifu/gui/MainGUI.java
@@ -0,0 +1,19 @@
+package waifu.gui;
+
+import java.util.List;
+import waifu.Anime;
+
+public class MainGUI implements View{
+	private Controller controller;
+	
+	public MainGUI(Controller controller){
+		this.controller=controller;
+	}
+	public void giveTags(List<String> tags){
+	
+	}
+	public void giveAnimes(List<Anime> animes){
+	
+	}
+
+}
