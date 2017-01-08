21aca9ef884d4598d076f23e59b622bef6660b22
xiul <xiul10901@gmail.com>
2017-01-08 11:50:30 +0100

    Creadas estructuras de datos necesarias para hacer la planificación

---
+++ src/waifu/Week.java
@@ -0,0 +1,25 @@
+package waifu;
+
+public class Week{
+	private int[] week;
+	public static final int MONDAY=0;
+	public static final int TUESDAY=1;
+	public static final int WEDNESDAY=2;
+	public static final int THURSDAY=3;
+	public static final int FRIDAY=4;
+	public static final int SATURDAY=5;
+	public static final int SUNDAY=6;
+	public static final int[] DAYS={MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY,SUNDAY};
+	public Week(){
+		week=new int[7]
+	}
+	public Week(int monday, int tuesday, int wednesday, int thursday, int friday, int saturday, int sunday,){
+		week={monday,tuesday,wednesday,thursday,friday,saturday,sunday};
+	}
+	
+	public int[] getWeek(){return week};
+	
+	public void setDay(int day,int value){week[day]=value;}
+	
+	public int getDay(int day){return week[day];}
+}
